10f15868dce7b99c83d298aea430c77db4f3b829
lolo <loic.aviez@proton.me>
2023-11-16 15:50:17 +0100

    corrigé

--- Faker.py
+++ Faker.py
@@ -10,45 +10,39 @@ def random_date(start, end):
 start_date = datetime(2023, 1, 1)
 end_date = datetime(2023, 6, 30)
 
-# Générer des données pour les utilisateurs
+# Générer des données pour 100 utilisateurs
+users_inserts = []
 users = []
-for _ in range(100):
+for user_id in range(1, 101):
     created_at = random_date(start_date, end_date)
-    users.append({
+    user = {
+        "user_id": user_id,
         "firstname": faker.first_name(),
         "lastname": faker.last_name(),
         "email": faker.email(),
         "username": faker.user_name(),
         "password": faker.password(),
         "created_at": created_at.strftime("%Y-%m-%d %H:%M:%S")
-    })
+    }
+    users.append(user)
+    users_inserts.append(f"INSERT INTO user_table (firstname, lastname, email, username, password, created_at) VALUES ('{user['firstname']}', '{user['lastname']}', '{user['email']}', '{user['username']}', '{user['password']}', '{user['created_at']}');\n")
 
 # Générer des données pour les vérifications d'email
 email_verifications = []
-for i in range(85):
-    user = random.choice(users)
+for user in users[:85]:
     verified_at = random_date(datetime.strptime(user["created_at"], "%Y-%m-%d %H:%M:%S"), end_date).strftime("%Y-%m-%d %H:%M:%S")
-    email_verifications.append({
-        "user_id": users.index(user) + 1,
-        "verified_at": verified_at
-    })
+    email_verifications.append(f"INSERT INTO user_email_verification_table (user_id, verified_at) VALUES ({user['user_id']}, '{verified_at}');\n")
 
 # Générer des données pour les sessions
 sessions = []
-for i in range(1000):
+for _ in range(1000):
     user = random.choice(users)
     user_created_at = datetime.strptime(user["created_at"], "%Y-%m-%d %H:%M:%S")
-    session_date = random_date(user_created_at, end_date).strftime("%Y-%m-%d %H:%M:%S")
-    sessions.append({
-        "user_id": users.index(user) + 1,
-        "connected_at": session_date
-    })
+    connected_at = random_date(max(user_created_at, datetime.strptime(user["created_at"], "%Y-%m-%d %H:%M:%S")), end_date).strftime("%Y-%m-%d %H:%M:%S")
+    sessions.append(f"INSERT INTO session_table (user_id, connected_at) VALUES ({user['user_id']}, '{connected_at}');\n")
 
-# Écrire les données dans le fichier SQL
+# Écriture des instructions SQL dans le fichier part_1_2.sql
 with open('part_1_2.sql', 'w') as file:
-    for user in users:
-        file.write(f"INSERT INTO \"user\" (firstname, lastname, email, username, password, created_at) VALUES ('{user['firstname']}', '{user['lastname']}', '{user['email']}', '{user['username']}', '{user['password']}', '{user['created_at']}');\n")
-    for verification in email_verifications:
-        file.write(f"INSERT INTO user_email_verification (user_id, verified_at) VALUES ({verification['user_id']}, '{verification['verified_at']}');\n")
-    for session in sessions:
-        file.write(f"INSERT INTO session (user_id, connected_at) VALUES ({session['user_id']}, '{session['connected_at']}');\n")
+    file.writelines(users_inserts)
+    file.writelines(email_verifications)
+    file.writelines(sessions)
